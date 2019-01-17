2a7fca38c5f7a4fd7832663fe5f4d8a4bd857744
heidiulery <heidiulery@hotmail.com>
2019-01-17 08:35:20 -0600

    fixed indentation in Album.js

--- src/components/Album.js
+++ src/components/Album.js
@@ -6,51 +6,87 @@ class Album extends Component {
 	constructor(props) {
 		super(props);
 
-		const album = albumData.find( album => {
+		const album = albumData.find(album => {
 			return album.slug === this.props.match.params.slug
 		});
 
 		this.state = {
-			album: album
+			album: album,
+			currentSong: album.songs[0],
+			isPlaying: false
 		};
+
+		this.audioElement = document.createElement('audio');
+		this.audioElement.src = album.songs[0].audioSrc;
 	}
 
-	render () {
+	play() {
+		this.audioElement.play();
+		this.setState({ isPlaying: true });
+	}
+
+	pause() {
+		this.audioElement.pause();
+		this.setState({ isPlaying: false });
+	}
+
+	setSong(song) {
+		this.audioElement.src = song.audioSrc;
+		this.setState({ currentSong: song });
+	}
+
+	handleSongClick(song) {
+		const isSameSong = this.state.currentSong === song;
+
+		if (this.state.isPlaying && isSameSong) {
+			this.pause();
+		} else {
+			if (!isSameSong) {
+				this.setSong(song);
+			}
+			this.play();
+		}
+	}
+
+	render() {
 		return (
 			<section className="album">
-        <section id="album-info">
-					<img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-           <div className="album-details">
-					 <h1 id="album-title">{this.state.album.title}</h1>
-					 <h2 className="artist">{this.state.album.artist}</h2>
-					 <div id="release-info">{this.state.album.releaseInfo}</div>
-           </div>
-				 </section>
-				 <table id="song-list" style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: '20px', marginBottom: '50px' }}>
-           <colgroup>
-             <col id="song-number-column" />
-             <col id="song-title-column" />
-             <col id="song-duration-column" />
-           </colgroup>
-					 <thead>
-							<th style={{ paddingRight: '20px' }}>Song Number</th>	 
-							<th>Song Title</th>	 
-							<th style={{ paddingLeft: '20px' }}>Song Duration</th>	 
-					 </thead>  
-           <tbody>
+				<section id="album-info">
+					<img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+					<div className="album-details">
+						<h1 id="album-title">{this.state.album.title}</h1>
+						<h2 className="artist">{this.state.album.artist}</h2>
+						<div id="release-info">{this.state.album.releaseInfo}</div>
+					</div>
+				</section>
+				<table id="song-list" style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: '20px', marginBottom: '50px' }}>
+					<colgroup>
+						<col id="song-number-column" />
+						<col id="song-title-column" />
+						<col id="song-duration-column" />
+					</colgroup>
+					<thead>
+						<th style={{ paddingRight: '20px' }}>Song Number</th>
+						<th>Song Title</th>
+						<th style={{ paddingLeft: '20px' }}>Song Duration</th>
+					</thead>
+					<tbody>
 						{
-							 this.state.album.songs.map( (song, index) => 
-								<tr key={ index }>
-									<td>{ index + 1 }</td>
-									<td>{ song.title }</td>
-									<td>{ song.duration }</td>
+							this.state.album.songs.map((song, index) =>
+								<tr
+									className="song"
+									key={index}
+									onClick={() => this.handleSongClick(song)}>
+									<td>{index + 1}</td>
+									<td>{song.title}</td>
+									<td>{song.duration}</td>
 								</tr>
-							 )	
+							)
 						}
-           </tbody>
-         </table>
-      </section>
-		
+					</tbody>
+				</table>
+			</section>
+
 		);
 	}
 }
