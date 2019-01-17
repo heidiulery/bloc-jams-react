6c15d6079942d276953d82b70c4584829df2cbcf
heidiulery <heidiulery@hotmail.com>
2019-01-16 18:43:32 -0600

    Components album checkpoint: completed

--- src/App.js
+++ src/App.js
@@ -10,16 +10,15 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-	  <nav>
-	    <Link to='/'>Landing</Link>
-	    <Link to='/library'>Library</Link>
-	  </nav>
+          <nav>
+            <Link to='/'>Landing</Link>
+            <Link to='/library'>Library</Link>
+          </nav>
           <h1>Bloc Jams</h1>
         </header>
         <main>
           <Route exact path="/" component={ Landing } />
           <Route path="/library" component={ Library } />
-          <Route path="/album" component={ Album } />
           <Route path="/album/:slug" component={ Album } />
         </main>
       </div>
--- src/components/Album.js
+++ src/components/Album.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import albumData from './../data/albums';
 
 class Album extends Component {
+	constructor(props) {
+		super(props);
+
+		const album = albumData.find( album => {
+			return album.slug === this.props.match.params.slug
+		});
+
+		this.state = {
+			album: album
+		};
+	}
+
 	render () {
 		return (
 			<section className="album">
-				{ this.props.match.params.slug }Album will go here
-			</section>
+        <section id="album-info">
+					<img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+           <div className="album-details">
+					 <h1 id="album-title">{this.state.album.title}</h1>
+					 <h2 className="artist">{this.state.album.artist}</h2>
+					 <div id="release-info">{this.state.album.releaseInfo}</div>
+           </div>
+				 </section>
+				 <table id="song-list" style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: '20px', marginBottom: '50px' }}>
+           <colgroup>
+             <col id="song-number-column" />
+             <col id="song-title-column" />
+             <col id="song-duration-column" />
+           </colgroup>
+					 <thead>
+							<th style={{ paddingRight: '20px' }}>Song Number</th>	 
+							<th>Song Title</th>	 
+							<th style={{ paddingLeft: '20px' }}>Song Duration</th>	 
+					 </thead>  
+           <tbody>
+						{
+							 this.state.album.songs.map( (song, index) => 
+								<tr key={ index }>
+									<td>{ index + 1 }</td>
+									<td>{ song.title }</td>
+									<td>{ song.duration }</td>
+								</tr>
+							 )	
+						}
+           </tbody>
+         </table>
+      </section>
+		
 		);
 	}
 }
